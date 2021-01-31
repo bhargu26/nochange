6d4ed0fe65e21297e663383a25d852421227d24a
bhargu26 <76743318+bhargu26@users.noreply.github.com>
2021-01-31 11:50:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -47,6 +47,10 @@ $(document).ready(function(){
         backSpeed: 60,
         loop: true
     });
+    var blink = document.getElementById('blink');
+    setInterval(function() {
+      blink.style.opacity = (blink.style.opacity == 0 ? 1 : 0);
+    }, 1000);
 
     // owl carousel script
     $('.carousel').owlCarousel({
